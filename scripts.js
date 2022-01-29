54291bdcee82edf602b373b34c08b8df34c92dba
Caio Senna <caiosenna93@gmail.com>
2022-01-29 14:58:09 -0300

    modal para visualizacao da imagem completa

--- scripts.js
+++ scripts.js
@@ -25,9 +25,7 @@ function getAPI(){
 
         }
         else{
-            $('#copy').text('No Author');         
-
-
+            $('#copy').text('No Author');     
         } 
 
 
@@ -42,8 +40,7 @@ function getAPI(){
             $('#ibagem').attr("src",result.url);
         }
         $('.container__bg').css('filter','blur(1.9px)');
-
-             
+    
           
     },
 
@@ -52,30 +49,15 @@ function getAPI(){
     }
 }
     );
-
-    
-
-    
+ 
 
 }
-
-
-
-
-
-
-
-
 
 
 function validacao(){
     let dataFmt=Date.parse($('#campo-data').val());
     let today=Date.now();
-    let min=Date.parse('1995-06-16');
-
-
-    
-
+    let min=Date.parse('1995-06-16'); 
 
     if(!$('#campo-data').val()){
         alert('Campo de data precisa ser preenchido!');
@@ -84,24 +66,41 @@ function validacao(){
     else if(dataFmt<min){
         alert('A data mínima para pesquisa é 1995-06-16.Por favor, tente novamente.');
         return false;
-
-
     } 
     
     else if(dataFmt>today){
         alert('A data máxima para pesquisa é o dia de hoje.Por favor, tente novamente.');
         return false;
 
-
     } 
     return true;
-
     
 }
 
 
 //1995-06-16
 //19950716
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("ibagem");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 
 
